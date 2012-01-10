c4243cb4000ae856660d805dc67db533d4f8a699
Diego Marcos <diego.marcos@gmail.com>
2012-01-09 18:54:22 -0800

    Adding requirejs compilation system

--- src/fitsPixelMapper.js
+++ src/fitsPixelMapper.js
@@ -1,4 +1,4 @@
-// Parses FITS pixel data and converts to the desired format
+// Parses FITS pixel data and converts fits photon count to the desired format
 // Author: Diego Marcos
 // Email: diego.marcos@gmail.com
 
